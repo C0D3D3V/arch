453aaa87fb9a515f8f7f2bd22cd46b5141e68c3b
c0d3d3v <live.for.program@gmail.com>
2020-03-19 21:35:31 +0100

    update update script

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+# This is a script to force update... Not recommended for usual updates
+
 # updating pgp keyring
 sudo pacman -Sy --noconfirm archlinux-keyring 
 sudo pacman-key --populate archlinux
@@ -9,6 +11,8 @@ yay -Syu --devel --timeupdate --noconfirm --noremovemake --cleanafter --norebuil
 
 # cleanup
 sudo pacman -Rcns --noconfirm $(pacman -Qtdq)
-# this will delete the comple package cache and is not recomanded to do usaly
+# this will delete the complete package cache and is not recommended to do usually
 sudo paccache -r -k 0
 sudo pacman -Scc --noconfirm
+
+
